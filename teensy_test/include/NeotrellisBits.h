eac3ad46cbf95811427958a64452b287553427ba
srperry96 <srperry96@gmail.com>
2021-11-18 17:47:36 +0000

    Small cleanup

--- teensy_test/include/NeotrellisBits.h
+++ teensy_test/include/NeotrellisBits.h
@@ -4,6 +4,9 @@
 #include <Arduino.h>
 #include "Adafruit_NeoTrellis.h"
 
+const uint8_t neotrellis_period = 20; //neotrellis has a resolution of around 60hz
+
+
 uint32_t Wheel(byte WheelPos);
 TrellisCallback blink(keyEvent evt);
 void setup_neotrellis();
--- teensy_test/src/main.cpp
+++ teensy_test/src/main.cpp
@@ -5,10 +5,8 @@
 
 #define MILLIS_PER_MIN 60000
 
-unsigned long start_millis, current_millis;
-const uint8_t neotrellis_period = 20; //neotrellis has a resolution of around 60hz
-
-unsigned long start_tempo_millis, current_tempo_millis;
+unsigned long neotrellis_millis, current_millis;
+unsigned long tempo_millis;
 
 void setup() {
     Serial.begin(9600);
@@ -22,8 +20,8 @@ void setup() {
     load_sample_filepaths();
 
     //get start millis for timing, instead of just using delays
-    start_millis = millis();
-    start_tempo_millis = millis();
+    neotrellis_millis = millis();
+    tempo_millis = millis();
 }
 
 uint8_t btn_count = 0;
@@ -34,32 +32,30 @@ bool metronome_on = false;
 
 
 void loop() {
-    current_millis = millis();
+  current_millis = millis();
 
-    current_tempo_millis = current_millis;
+  if(metronome_on){
+    if(current_millis - tempo_millis > metronome_period){
+      play_metronome();
+      tempo_millis = current_millis;
+    }
+  }
+
+
+
+  //Read neotrellis at the specified rate
+  if(current_millis - neotrellis_millis > neotrellis_period){
+    trellis.read();
   
-    if(metronome_on){
-      if(current_tempo_millis - start_tempo_millis > metronome_period){
-        play_metronome();
-        start_tempo_millis = current_tempo_millis;
+    //check pad states, play corresponding sample if needed
+    for(btn_count = 0; btn_count < 16; btn_count++){
+      if(button_states[btn_count] == 1){
+        play_pad_sample(btn_count);
+
+        //reset the button state so we dont double play the sample
+        button_states[btn_count] = 0;
       }
     }
-
-
-
-    //Read neotrellis at the specified rate
-    if(current_millis - start_millis > neotrellis_period){
-        trellis.read();
-    
-        //check pad states, play corresponding sample if needed
-        for(btn_count = 0; btn_count < 16; btn_count++){
-          if(button_states[btn_count] == 1){
-            play_pad_sample(btn_count);
-
-            //reset the button state so we dont double play the sample
-            button_states[btn_count] = 0;
-          }
-        }
-        start_millis = current_millis;
-    }
+    neotrellis_millis = current_millis;
+  }
 }
